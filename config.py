7ae4d0f0e63f49a2fa6cbc0ac83ad76f0846267c
Alexey Anosov <a.anosov@verweb.dev>
2024-03-25 06:22:51 +0000

    Added requirements versions

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-BUCKET_KEY_ID='YCAJEHlaVMYbUsaLZwiMcqMoL'
-BUCKET_KEY='YCNHyv7Y6h0f5JH2VBfcmDhv6C_gaMcFArouf_LT'
+BUCKET_KEY_ID='YCAJE2uTroLAdoatdbs9wgL3S'
+BUCKET_KEY='YCOzFjg91RTaArmwi-ZBVCTQA_Vd1ibjeh3ij5nZ'
 BUCKET_NAME='itmo-bigdata-lab1'
 BUCKET_URL='https://storage.yandexcloud.net/clear'
