b24ad1c8468306a8194f64c974dde5885414826e
Alexej Anosov <alexej-anosov@gmail.com>
2024-09-22 12:16:23 +0000

    small fix

--- src/kafka/consumer_service.py
+++ src/kafka/consumer_service.py
@@ -1,17 +1,15 @@
 import asyncio
 from consumer import KafkaConsumer
 
-# Initialize the KafkaConsumer
+
 kafka_consumer = KafkaConsumer(
-    servers='kafka:9092',  # Kafka service inside Docker network
+    servers='kafka:9092',  
     group_id='save_to_db_group',
     topic='save_to_db'
 )
 
-# Main function to run the consumer
 async def main():
     await kafka_consumer.consume()
 
 if __name__ == '__main__':
-    # Start the consumer
     asyncio.run(main())
